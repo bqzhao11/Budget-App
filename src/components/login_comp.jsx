61fc5046e29c92d8dc70825c0727000c234634c7
Alex Che <https://github-dev.cs.illinois.edu/>
2021-01-10 16:00:41 -0600

    New button

--- src/components/login_comp.jsx
+++ src/components/login_comp.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
 export default class Treasurer extends React.Component {
   render() {
     return (
       <div>
-        <h1>This is the Login Page</h1>
+        <h1> Welcom To DKE App MOTHERA FUCKERA </h1>
+        <button className = "btn btn-primary"> Login With Google </button>
       </div>
     );
   }
