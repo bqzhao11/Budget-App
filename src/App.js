4c5a7277f1a5960a0d71ee88bb54f7e65ed1f221
Ben Zhao <bqzhao11@gmail.com>
2021-01-10 15:21:48 -0500

    added react template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import Dashboard from "./components/dashboard_comp";
+import Treasurer from "./components/treasurer_comp";
+import Login from "./components/login_comp";
 class App extends React.Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
+            <Router>
+                <div className="App">
+                    <Route path="/dashboard" component={Dashboard} />
+                    <Route path="/treasurer" component={Treasurer} />
+                    <Route path="/login" component={Login} />
+                </div>
+            </Router>
         );
     }
 }
---
+++ src/components/dashboard_comp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Treasurer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the Dashboard Page</h1>
+      </div>
+    );
+  }
+}
