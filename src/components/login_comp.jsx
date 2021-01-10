abcd8e580baf317ab413a7e8e8eb4b88d84c9875
Alex Che <https://github-dev.cs.illinois.edu/>
2021-01-10 16:39:32 -0600

    Added Images

--- src/components/login_comp.jsx
+++ src/components/login_comp.jsx
@@ -3,8 +3,11 @@ export default class Treasurer extends React.Component {
   render() {
     return (
       <div>
-        <h1> Welcom To DKE App MOTHERA FUCKERA </h1>
-        <button className = "btn btn-primary"> Login With Google </button>
+        <img src="https://i.pinimg.com/originals/91/07/4b/91074bd8da17b085863a262c88197c95.png" className = "h-75 d-inline-block"></img>
+        <div className = "position-absolute top-50 start-50 translate-middle">
+          <h1 strong > Welcome To The DKE App </h1>
+          <button className = "btn btn-primary btn-lg" strong> Login With Google </button>
+        </div>
       </div>
     );
   }
