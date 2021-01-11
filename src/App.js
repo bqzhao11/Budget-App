a1a356582acb817b56000ba5a5c46be868267d55
Ben Zhao <bqzhao11@gmail.com>
2021-01-11 14:37:59 -0500

    added login with google button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+
 import Dashboard from "./components/dashboard_comp";
 import Treasurer from "./components/treasurer_comp";
 import Login from "./components/login_comp";
+
 class App extends React.Component {
     render() {
         return (
