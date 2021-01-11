849eb2071174e5d3e54e752959a7b345fb840581
harsh <harshbp2@illinois.edu>
2021-01-10 18:21:29 -0600

    ok

--- src/components/dashboard_comp.jsx
+++ src/components/dashboard_comp.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import UserInterFace from "./user_interface_comp";
 
 export default class Treasurer extends React.Component {
   render() {
     return (
       <div>
         <h1>This is the Dashboard Page</h1>
+        <UserInterFace/>
       </div>
     );
   }
