680ae5420635a9e52bfa0f6c8aa38e2b012b9859
Alex Che <https://github-dev.cs.illinois.edu/>
2021-01-11 00:20:39 -0600

    Making new login Branch

--- src/components/login_comp.jsx
+++ src/components/login_comp.jsx
@@ -4,7 +4,7 @@ export default class Treasurer extends React.Component {
   render() {
     return (
         <div className = "position">
-          <h1> Welcome To The DKE App </h1>
+          <h1> Welcome To The DKE App HEHE </h1>
           <button className = "btn btn-primary btn-lg" strong> Login With Google </button>
         </div>
     );
