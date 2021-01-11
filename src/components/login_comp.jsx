a08540654dce20262af29fcbc0626f2423ffa5dd
Ben Zhao <bqzhao11@gmail.com>
2021-01-11 14:45:43 -0500

    fixed merge errors

--- src/components/login_comp.jsx
+++ src/components/login_comp.jsx
@@ -6,13 +6,16 @@ import LoginButton from "./login_button";
 export default class Treasurer extends React.Component {
   render() {
     return (
-      <body className = "loginBody">
-      <main>
-          <div className = "main">
-            <h1 className = "sign"> Welcome </h1>
-            <button class="submit" align="center">Sign In With Google</button>
+      <body className="loginBody">
+        <main>
+          <div className="main">
+            <h1 className="sign"> Welcome </h1>
+            <LoginButton />
+            <button class="submit" align="center">
+              Sign In With Google
+            </button>
           </div>
-      </main>
+        </main>
       </body>
     );
   }
