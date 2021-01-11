c20cbe3b7f5ef75450d3735c1fc9a5c61d7ecf31
Ben Zhao <bqzhao11@gmail.com>
2021-01-11 18:39:01 -0500

    google login connected with database and dashboard

--- src/components/login_button.jsx
+++ src/components/login_button.jsx
@@ -1,30 +1,60 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 import { GoogleLogin } from "react-google-login";
 import { GOOGLE_CLIENT_ID } from "./../config.json";
+
 require("dotenv").config();
 
 const clientId = GOOGLE_CLIENT_ID;
 
-export default function Login() {
-  const onSuccess = (res) => {
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+      gmail: "",
+    };
+
+    this.onSuccess = this.onSuccess.bind(this);
+    this.onFailure = this.onFailure.bind(this);
+  }
+  onSuccess = (res) => {
     console.log("[Login Success] currentUser:", res.profileObj);
+    console.log(res.profileObj.email);
+    this.setState({
+      loggedIn: true,
+      gmail: res.profileObj.email,
+    });
+    <Redirect
+      to={{
+        pathname: "/dashboard",
+        state: { gmail: res.profileObj.email },
+      }}
+    />;
   };
 
-  const onFailure = (res) => {
+  onFailure = (res) => {
     console.log("[Login failed] res:", res);
   };
-
-  return (
-    <div>
-      <GoogleLogin
-        clientId={clientId}
-        buttonText="Login"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-        cookiePolicy={"single_host_origin"}
-        style={{ marginTop: "100px" }}
-        isSignedIn={true}
-      />
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        {this.state.loggedIn ? (
+          <Redirect
+            to={{ pathname: "/dashboard", state: { gmail: this.state.gmail } }}
+          />
+        ) : (
+          <GoogleLogin
+            clientId={clientId}
+            buttonText="Login"
+            onSuccess={this.onSuccess}
+            onFailure={this.onFailure}
+            cookiePolicy={"single_host_origin"}
+            style={{ marginTop: "100px" }}
+            isSignedIn={true}
+          />
+        )}
+      </div>
+    );
+  }
 }
