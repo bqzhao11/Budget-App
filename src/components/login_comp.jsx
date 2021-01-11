4d18ba2d4dc0194cb18f8db78a16da809f3990e1
Alex Che <https://github-dev.cs.illinois.edu/>
2021-01-11 13:39:20 -0600

    Fixed bugs

--- src/components/login_comp.jsx
+++ src/components/login_comp.jsx
@@ -3,16 +3,14 @@ import "../css/login.css";
 export default class Treasurer extends React.Component {
   render() {
     return (
-        // <div className = "position">
-        //   <h1 className = "titleLogin"> Welcome To The DKE App </h1>
-        //   <h2 className = "title2"> Sign In</h2>
-        //   <button className = "btn btn-primary btn-lg" strong> Login With Google </button>
-        // </div>
+      <body className = "loginBody">
+      <main>
           <div className = "main">
             <h1 className = "sign"> Welcome </h1>
             <button class="submit" align="center">Sign In With Google</button>
           </div>
-        
+      </main>
+      </body>
     );
   }
 }
